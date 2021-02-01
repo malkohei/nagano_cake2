9bbe2a1231df051969a33348554191d55a1a69b9
malkopoke12345@gmail.com <ec2-user@ip-172-31-94-149.ec2.internal>
2021-02-01 16:06:35 +0000

    header update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,33 @@
 Rails.application.routes.draw do
 
+  scope module: :public do
+
+    resources :items, only: [:index, :show]
+
+    resources :customer, only: [:show, :edit, :update] do
+      collection do
+        get 'quit'
+        patch 'out'
+      end
+    end
+
+    resources :cart_items, only: [:index, :update, :create, :destroy] do
+      collection do
+        delete 'all_destroy'
+      end
+    end
+
+    resources :orders, only: [:new, :create, :index, :show] do
+      collection do
+        post 'confirm'
+        get 'thanks'
+      end
+    end
+  end
+
+  root 'public/homes#top'
+  get '/about' => 'public/homes#about'
+
   namespace :admin do
    resources :items, only: [:index, :new, :show, :edit, :create, :update]
    resources :genres, only: [:index, :edit, :create, :update]
@@ -19,6 +47,6 @@ Rails.application.routes.draw do
     passwords: 'customers/passwords'
   }
 
-  get '/admin' => 'homes#top'
+  get '/admin' => 'admin/homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
